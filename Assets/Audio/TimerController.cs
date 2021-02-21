f1be350bb98259c312587c667dffe21033513e4a
chevre hurlante lol <paul.breuza@gmail.com>
2021-02-21 19:31:18 +0100

    Timer done

---
+++ Assets/Audio/TimerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerController : MonoBehaviour
+{
+    public static TimerController instance;
+
+    public Text timeCounter;  //serialze
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+    private float elapsedTime;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+       timeCounter.text = "00:00.00";
+       timerGoing = false;
+       TimerController.instance.BeginTimer();
+
+    }
+
+    public void BeginTimer()
+    {
+        elapsedTime = 0f;
+        timerGoing = true;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = timePlaying.ToString("mm':'ss'.'ff");
+            timeCounter.text = timePlayingStr;
+            yield return null;
+        }
+    } 
+}
