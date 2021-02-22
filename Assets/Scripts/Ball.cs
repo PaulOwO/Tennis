5215e408dbdf5164164883adb126c0e6c875a013
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-02-22 17:24:14 +0100

    added pong aspects of the game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 3f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        initializeBallVelocity();
+    }
+
+    void initializeBallVelocity()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(x*speed, y*speed);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    public bool isPlayer;
+
+    public float speed;
+
+    public Rigidbody2D rb;
+
+    private float movement;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isPlayer)
+        {
+            movement = Input.GetAxisRaw("Paddle");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+}
