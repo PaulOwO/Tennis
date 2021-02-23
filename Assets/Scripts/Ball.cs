4481341bafa18a34f934978ae615186262ef7677
Aidan Cuennet <aidan.cuennet@gmail.com>
2021-02-23 17:44:01 +0100

    commit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private float x;
+    private float y;
     [SerializeField] AudioSource audioSource;
     public float speed = 3f;
     public Rigidbody2D rb;
@@ -11,19 +13,33 @@ public class Ball : MonoBehaviour
     void Start()
     {
         startPosition = transform.position;
-        initializeBallVelocity();
+        Launch();
     }
 
-    void initializeBallVelocity()
+    private void Launch()
     {
-        float x = Random.Range(0, 2) == 0 ? -1 : 1;
-        float y = Random.Range(0, 2) == 0 ? -1 : 1;
-        rb.velocity = new Vector2(x * speed, y * speed);
+        if (Random.Range(0, 2) == 0)
+        {
+            x = -1;
+        }
+        else
+        {
+            x = 1;
+        }
+        if (Random.Range(0, 2) == 0)
+        {
+            y = -1;
+        }
+        else
+        {
+            y = 1;
+        }
     }
 
 
     void Update()
     {
+        rb.velocity = new Vector2 (speed, speed);
         Vector3 viewPos = UnityEngine.Camera.main.WorldToViewportPoint(transform.position);
         bool isVisible = true;
         if (viewPos.x >= 0 && viewPos.x <= 1 && viewPos.y >= 0 && viewPos.y <= 1 && viewPos.z > 0)
@@ -39,7 +55,7 @@ public class Ball : MonoBehaviour
         {
             Debug.Log("you have lost");
             transform.position = startPosition;
-            initializeBallVelocity();
+            Launch();
             //timerGoing = false;
         }
     }
@@ -48,7 +64,7 @@ public class Ball : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
-            speed = speed + 1;
+            speed = speed + 0.25f;
             audioSource.Play();
         }
     }
